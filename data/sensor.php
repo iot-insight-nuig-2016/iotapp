f783ecb3444196f3f7d3e3a623710b93b920f312
Rathachai Chawuthai <rathachai.c@gmail.com>
2016-09-09 18:31:27 +0100

    ui

---
+++ data/sensor.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-type: application/json');
+
+$arr = array();
+
+for($h = 0; $h<24; $h++){
+
+	$hh = $h."h";
+
+
+	$arr[$hh] = array();
+
+	for($room = 10; $room <20; $room ++){
+		$value = rand(5,100)/100;
+		$roomname = "room-".$room;
+		//$arr[$hh][$roomname] = $value;
+		$arr[$hh][] = array("label"=>$roomname, "value"=>$value);
+	}
+
+}
+
+echo json_encode($arr);
+
+?>
